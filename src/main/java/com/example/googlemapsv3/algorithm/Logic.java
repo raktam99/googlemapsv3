c5a1b895909bbe2a36899eabd50ac8a1a8c5abcb
raktam99 <raktam78@gmail.com>
2023-11-21 21:24:32 +0100

    Handling connection error

--- src/main/java/com/example/googlemapsv3/algorithm/Logic.java
+++ src/main/java/com/example/googlemapsv3/algorithm/Logic.java
@@ -17,44 +17,51 @@ import com.google.gson.reflect.TypeToken;
 public class Logic {
     private static List<String> waypoints = new ArrayList<>();
     public static void getResponse(){
-        waypoints = new ArrayList<>();
+        try {
+            waypoints = new ArrayList<>();
 
-        HttpClient httpClient = HttpClient.newHttpClient();
+            HttpClient httpClient = HttpClient.newHttpClient();
 
-        Gson gson = new Gson();
+            Gson gson = new Gson();
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8080/api/shipments/0/getAllSorted"))
-                .header("Content-Type", "application/json")
-                .GET()
-                .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/api/shipments/0/getAllSorted"))
+                    .header("Content-Type", "application/json")
+                    .GET()
+                    .build();
 
-        CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+            CompletableFuture<HttpResponse<String>> response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
 
-        response.thenApply(HttpResponse::body)
-                .thenAccept( json -> {
-                    Type listType = new TypeToken<List<Shipment>>(){}.getType();
-                    List<Shipment> shipments = gson.fromJson(json, listType);
+            response.thenApply(HttpResponse::body)
+                    .thenAccept(json -> {
+                        Type listType = new TypeToken<List<Shipment>>() {
+                        }.getType();
+                        List<Shipment> shipments = gson.fromJson(json, listType);
 
-                    if(shipments == null || shipments.isEmpty()) return;
+                        if (shipments == null || shipments.isEmpty()) return;
 
-                    System.out.println("From: " + shipments.get(0).getOrigin() + "\nTo:");
-                    waypoints.add(shipments.get(0).getOrigin());
-                    for (Shipment sh: shipments) {
-                        System.out.println(sh.getDestination() + " " + sh.getShipmentId());
-                        waypoints.add(sh.getDestination());
-                    }
-                })
-                .join();
+                        System.out.println("From: " + shipments.get(0).getOrigin() + "\nTo:");
+                        waypoints.add(shipments.get(0).getOrigin());
+                        for (Shipment sh : shipments) {
+                            System.out.println(sh.getDestination() + " " + sh.getShipmentId());
+                            waypoints.add(sh.getDestination());
+                        }
+                    })
+                    .join();
 
-        if(waypoints == null || waypoints.isEmpty()){
-            MainController.getHelloController().txtOrigin.setText("No scheduled shipments!");
-            MainController.getHelloController().txtWaypoints.setText("");
-            return;
+            if (waypoints == null || waypoints.isEmpty()) {
+                MainController.getHelloController().txtOrigin.setText("No scheduled shipments!");
+                MainController.getHelloController().txtWaypoints.setText("");
+                return;
+            }
+
+            MainController.getHelloController().txtOrigin.setText(waypoints.get(0));
+            MainController.getHelloController().txtWaypoints.
+                    setText(String.join(";", waypoints.subList(1, waypoints.size())));
         }
-
-        MainController.getHelloController().txtOrigin.setText(waypoints.get(0));
-        MainController.getHelloController().txtWaypoints.
-                setText(String.join(";", waypoints.subList(1, waypoints.size())));
+        catch (Exception ex){
+            MainController.getHelloController().txtOrigin.setText("Connection error!");
+            MainController.getHelloController().txtWaypoints.setText("");
+        }
     }
 }
