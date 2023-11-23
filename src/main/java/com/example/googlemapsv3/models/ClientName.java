bb8c79ce4692c61bd1306b3c82221e71101d2e85
raktam99 <raktam78@gmail.com>
2023-11-23 20:24:24 +0100

    Cryptogramphy

---
+++ src/main/java/com/example/googlemapsv3/models/ClientName.java
@@ -0,0 +1,13 @@
+package com.example.googlemapsv3.models;
+
+public class ClientName {
+    private String clientName;
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+}
---
+++ src/main/java/com/example/googlemapsv3/models/ClientNameAndPublicKey.java
@@ -0,0 +1,23 @@
+package com.example.googlemapsv3.models;
+
+public class ClientNameAndPublicKey {
+    private String clientName;
+
+    private String publicKey;
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+}
